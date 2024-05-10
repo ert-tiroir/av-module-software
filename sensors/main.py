ea3951b40686197536d67fd8066fc8d4cb7b048c
Mr Thimote <mrthimote@gmail.com>
2024-05-10 11:46:46 +0200

    added time to telemetry

--- sensors/main.py
+++ sensors/main.py
@@ -5,7 +5,7 @@ from adafruit_dps310.basic import DPS310
 from core import CoreTarget
 from logger import ModuleLogger, SUCCESS, INFO
 
-from utils import pack_float
+from utils import pack_float, pack_int
 
 i2c = board.I2C()   # uses board.SCL and board.SDA
 
@@ -27,6 +27,7 @@ while True:
     target.write_string_to_core(
         bytes(
             [1]
+          + pack_int  (int(time.time() * 1000000), 8)
           + pack_float(dps310.temperature)
           + pack_float(dps310.pressure)
           + pack_float(dps310.altitude)
--- sensors/utils.py
+++ sensors/utils.py
@@ -1,5 +1,11 @@
 
 import struct
 
+def pack_int (value, count):
+    L = []
+    for i in range(count):
+        L.append(value % 256)
+        value //= 256
+    return L
 def pack_float (float):
     return list(reversed(struct.pack("!f", float)))
